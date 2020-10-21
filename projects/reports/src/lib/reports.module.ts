aae0a591559a80bc138d6bfd8b35da4340036c43
Your Name <you@example.com>
2020-10-21 17:19:10 +0300

    initial comii

---
+++ projects/reports/src/lib/reports.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ReportsComponent } from './reports.component';
+
+
+
+@NgModule({
+  declarations: [ReportsComponent],
+  imports: [
+  ],
+  exports: [ReportsComponent]
+})
+export class ReportsModule { }
---
+++ projects/reports/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of reports
+ */
+
+export * from './lib/reports.service';
+export * from './lib/reports.component';
+export * from './lib/reports.module';
