6bfaa766211262a9ebb76b87bc55aa1b5b448615
mecarito <pieritomecar@gmail.com>
2021-01-19 15:21:16 +0300

    made minor changes

--- projects/reports/src/pages/reorder-report.page.ts
+++ projects/reports/src/pages/reorder-report.page.ts
@@ -1,61 +1,65 @@
-import {Component, OnInit} from '@angular/core';
-import {DeviceInfoUtil} from '@smartstocktz/core-libs';
+import { Component, OnInit } from '@angular/core';
+import { DeviceInfoUtil } from '@smartstocktz/core-libs';
 
 @Component({
   selector: 'smartstock-stock-reports',
   template: `
-      <div>
-          <mat-sidenav-container class="my-drawer-container">
+    <div>
+      <mat-sidenav-container class="my-drawer-container">
+        <mat-sidenav
+          [fixedInViewport]="true"
+          class="match-parent-side"
+          #sidenav
+          [mode]="enoughWidth() ? 'side' : 'over'"
+          [opened]="enoughWidth()"
+        >
+          <smartstock-drawer></smartstock-drawer>
+        </mat-sidenav>
 
-              <mat-sidenav
-                      [fixedInViewport]="true"
-                      class="match-parent-side"
-                      #sidenav [mode]="enoughWidth()?'side':'over'"
-                      [opened]="enoughWidth()">
-                  <smartstock-drawer></smartstock-drawer>
-              </mat-sidenav>
+        <mat-sidenav-content>
+          <smartstock-toolbar
+            [heading]="'Stock Reports'"
+            [sidenav]="sidenav"
+            [showProgress]="false"
+          ></smartstock-toolbar>
 
-              <mat-sidenav-content>
-                  <smartstock-toolbar [heading]="'Stock Reports'" [sidenav]="sidenav" [showProgress]="false"></smartstock-toolbar>
-
-                    <div class="row m-0">
-                      <div class="col-lg-6 py-3">
-                        <smartstock-expired-products-report></smartstock-expired-products-report>
-                      </div>
-                      <div class="col-lg-6 py-3">
-                        <smartstock-products-about-to-expire></smartstock-products-about-to-expire>
-                      </div>
-                    </div>
-                    <div class="row m-0">
-                      <div class="col-lg-6 py-3">
-                        <smartstock-stock-reorder-report></smartstock-stock-reorder-report>
-                      </div>
-                      <div class="col-lg-6 py-3"></div>
-                    </div>
-<!--                  <div [ngStyle]="{padding: (isMobile || !enoughWidth())?'24px 0':'24px 16px'}"-->
-<!--                       [ngClass]="(isMobile || !enoughWidth())?'container-fluid':'container'">-->
-<!--                      <div class="col-12 col-lg-10 col-xl-10 offset-xl-1 offset-lg-1 offset-md-0 offset-sm-0">-->
-<!--                          <div class="row">-->
-<!--                              <div style="margin-bottom: 10px" class="col-12">-->
-<!--                              </div>-->
-<!--                          </div>-->
-<!--                      </div>-->
-<!--                  </div>-->
-
-              </mat-sidenav-content>
-          </mat-sidenav-container>
-      </div>
+          <div class="row m-0">
+            <div class="col-lg-6 py-3">
+              <smartstock-expired-products-report></smartstock-expired-products-report>
+            </div>
+            <div class="col-lg-6 py-3">
+              <smartstock-products-about-to-expire></smartstock-products-about-to-expire>
+            </div>
+          </div>
+          <div class="row m-0">
+            <div class="col-lg-6 py-3">
+              <smartstock-stock-reorder-report></smartstock-stock-reorder-report>
+            </div>
+            <div class="col-lg-6 py-3"></div>
+          </div>
+          <!--                  <div [ngStyle]="{padding: (isMobile || !enoughWidth())?'24px 0':'24px 16px'}"-->
+          <!--                       [ngClass]="(isMobile || !enoughWidth())?'container-fluid':'container'">-->
+          <!--                      <div class="col-12 col-lg-10 col-xl-10 offset-xl-1 offset-lg-1 offset-md-0 offset-sm-0">-->
+          <!--                          <div class="row">-->
+          <!--                              <div style="margin-bottom: 10px" class="col-12">-->
+          <!--                              </div>-->
+          <!--                          </div>-->
+          <!--                      </div>-->
+          <!--                  </div>-->
+        </mat-sidenav-content>
+      </mat-sidenav-container>
+    </div>
   `,
-  styleUrls: ['../styles/stock.style.scss']
+  styleUrls: ['../styles/stock.style.scss'],
 })
-export class ReorderReportPageComponent extends DeviceInfoUtil implements OnInit {
+export class ReorderReportPageComponent
+  extends DeviceInfoUtil
+  implements OnInit {
   isMobile = false;
 
   constructor() {
     super();
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
